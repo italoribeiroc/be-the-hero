392287c0ea2a0aff701e5c3af405c4d2b62d45cf
Italo Ribeiro <italovianelli59@hotmail.com>
2021-05-08 01:10:01 -0300

    Adição de testes

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,17 +1,17 @@
+const generateUniqueId = require('../utils/generateUniqueId');
 const connection = require('../database/connection');
-const crypto = require('crypto');
 
 module.exports = {
-    async index(request, response){
+    async index(request, response) {
         const ongs = await connection('ongs').select('*');
-    
+
         return response.json(ongs);
     },
 
-    async create(request, response){
-        const {name, email, whatsapp, city, uf} = request.body;
+    async create(request, response) {
+        const { name, email, whatsapp, city, uf } = request.body;
 
-        const id = crypto.randomBytes(4).toString('HEX');
+        const id = generateUniqueId();
 
         await connection('ongs').insert({
             id,
